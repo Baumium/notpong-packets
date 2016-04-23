9823b67eec58388910b60ebb7328171fd7385383
DaPasta <484902@gmail.com>
2016-04-22 23:31:21 -0700

    Add movement and lobby related packets

--- src/main/java/com/dapasta/notpong/packets/client/MovementRequest.java
+++ src/main/java/com/dapasta/notpong/packets/client/MovementRequest.java
@@ -3,9 +3,11 @@ package com.dapasta.notpong.packets.client;
 import com.dapasta.notpong.packets.Packet;
 
 public class MovementRequest extends Packet {
+    public int id;
+
     public String sessionId;
     public int playerId;
 
-    public float dx;
+    public float x;
 
 }
--- src/main/java/com/dapasta/notpong/packets/server/MovementResponse.java
+++ src/main/java/com/dapasta/notpong/packets/server/MovementResponse.java
@@ -3,5 +3,6 @@ package com.dapasta.notpong.packets.server;
 import com.dapasta.notpong.packets.Packet;
 
 public class MovementResponse extends Packet {
+    public int id;
     public float x;
 }
